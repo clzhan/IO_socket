5fe7e3f989dcb9d7c03c20de5559fe3dbb4f5eb6
Your Name <you@example.com>
2018-10-18 20:09:35 -0700

    add demo
    Signed-off-by: Your Name <you@example.com>

---
+++ poll/client/client.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+
+#include <arpa/inet.h>
+
+#include <string.h>
+#include <unistd.h>
+
+
+
+
+
+int main(int argc, char * argv[])
+{
+	//create socket
+	int socketfd = 0;
+
+	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(socketfd < 0)
+	{
+		printf("create socketfd error: %s errno %d\n", strerror(errno),errno);
+		return -1;
+	}
+
+	
+	//connect 
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(6000);
+
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	if(connect(socketfd, (struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
+	{
+		printf("connect error: %s errno %d\n", strerror(errno),errno);
+		return -1;
+
+	}
+
+	
+	char sendbuf[1024] = {0};
+	char recvbuf[1024] = {0};
+
+	while(fgets(sendbuf, sizeof(sendbuf), stdin) != NULL)
+	{
+		write(socketfd, sendbuf, strlen(sendbuf));
+
+		int ret = read(socketfd, recvbuf, sizeof(recvbuf));
+		
+		if(0 == ret)
+		{
+			break;
+		}
+
+
+
+		fputs(recvbuf, stdout);
+
+		memset(sendbuf, 0, sizeof(sendbuf));
+		memset(recvbuf, 0, sizeof(recvbuf));
+
+	}
+	
+	close(socketfd);
+
+	return 0;
+}
--- poll/server/server.cpp
+++ poll/server/server.cpp
@@ -9,6 +9,7 @@
 
  #include <poll.h>
 #include <vector>
+#include <errno.h>
 
 
 
@@ -22,13 +23,15 @@ int main(int argc, char * argv[])
 	//1. create socket
 	int socketfd  = 0;
 
-	if(socketfd = socket(AF_INET, SOCK_STREAM, 0) < 0)
+	socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(socketfd <= 0)
 	{
 		fprintf(stderr, "create socket failed!");
+		printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
 		return -1;
 	}
 
-
+	printf("socketfd = %d\n", socketfd);
 
 	//2. bind
 	struct sockaddr_in servaddr;
@@ -36,13 +39,14 @@ int main(int argc, char * argv[])
 
 	//set value
 	servaddr.sin_family= AF_INET;
-	servaddr.sin_port = htons(5188);
+	servaddr.sin_port = htons(6000);
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 
 	if(bind(socketfd, (struct sockaddr *) &servaddr, sizeof(struct sockaddr)) < 0)
 	{
 		perror("failed to bind");
+		printf("bind error: %s(errno: %d)\n", strerror(errno), errno);
 		close(socketfd);
 		return -1;
 	}
@@ -91,10 +95,10 @@ int main(int argc, char * argv[])
 			continue;
 		}
 
-		
+		printf("nready %d\n", nready);		
 		//first element is socketfd
 
-		if(pollfds[0].revents & POOLIN)
+		if(pollfds[0].revents & POLLIN)
 		{
 			peerlen = sizeof(peeraddr);
 			connfd = accept(socketfd, (struct sockaddr*)&peeraddr, &peerlen);
@@ -105,22 +109,61 @@ int main(int argc, char * argv[])
 				close(socketfd);
 				return -1;
 			}
-
+			
 
 			pfd.fd = connfd;
 			pfd.events= POLLIN;
 			pfd.revents = 0;
 			pollfds.push_back(pfd);
+
 			nready --;
 
 
-			printf("has a connect come")
+			printf("has a connect coming remote ip %s port %d connfd %d\n",inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port),connfd);
+
+			if(nready == 0)
+			{
+				continue;
+			}
 
 		}
 
+		for(PollFdList::iterator itor = pollfds.begin() + 1 ; 
+				itor != pollfds.end() && nready > 0 ; 
+				++itor)
+		{
+			if(itor->revents & POLLIN)
+			{
 
+				printf("read a buff .....\n");
+				--nready;
+				connfd = (*itor).fd;
 
+				char buf[1024] = {0};
 
+				int ret = read(connfd, buf, 1024);
+
+				if(-1 == ret)
+				{
+					perror("recv error");
+					return -1;
+				}
+
+				if(0 == ret)
+				{
+					printf("Client close ...");
+					itor = pollfds.erase(itor);
+					--itor;
+
+					close(connfd);
+					continue;
+				}
+
+				printf("%s\n", buf);
+				write(connfd, buf, strlen(buf));
+
+			}
+		}
 	}
 
 	
